865cfc1769e60d0ed4055c00abe1a48d8805740c
Felicia Zheng <felicia0zheng@gmail.com>
2024-04-07 17:33:52 +0000

    updated filter

--- src/EventPage.js
+++ src/EventPage.js
@@ -6,42 +6,36 @@ import "./EventCat.css";
 import data from "./data.store";
 
 export default function EventCat({ currentCategory, setCurrentCategory }) {
-  let categories = [];
-  const [filteredEventData, setFilteredEventData] = useState([]);
+  const [eventData, setEventData] = useState([]);
 
   useEffect(() => {
-    fetchCategories();
-    console.log("Event Data:", eventData);
+    fetchEventDataAndUpdateState();
   }, []);
 
   useEffect(() => {
-    if (data.currentData.category) {
-      filterEventData(data.currentData.category);
-    }
-  }, [data]);
+    filterEventData(data.currentData.category);
+    console.log(data.currentData.category, "cat");
+  }, [data.currentData.category]);
 
-  const fetchCategories = async () => {
+  const fetchEventDataAndUpdateState = async () => {
     const eventData = await fetchEventData();
-    categories = eventData;
-    console.log("Event Data:", eventData);
+    setEventData(eventData);
+    
   };
 
   const filterEventData = (category) => {
-    // Filter eventData based on the specified category
-    console.log("Categories:", categories);
-    const filteredEvents = categories.filter(
-      (row) => row.Category === category,
+    console.log("event event data", eventData);
+    console.log("rvrvr");
+    const filteredEvents = eventData.filter(
+      (event) => event.Category === category,
     );
-
-    console.log("Filtered Categories:", filteredEvents);
-    // Extract EventName objects from filtered events
+    console.log(filteredEvents);
     const eventNames = filteredEvents.map((event) => event.EventName);
-
-    // Set the filtered event names in state
-    categories = eventNames.slice(0, 4);
-
-    console.log("Filtered Event Names:", eventNames.slice(0, 4));
+    setFilteredEventData(eventNames.slice(0, 4));
+    console.log("Filtered Event Data:", eventNames.slice(0, 4));
   };
+
+  const [filteredEventData, setFilteredEventData] = useState([]);
 
   return (
     <>
